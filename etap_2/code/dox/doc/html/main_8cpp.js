b5de1111a4fede7cd6050da4ba7723cda479db6d
Bartosz Piech <piech.bartosz696@gmail.com>
2021-11-30 18:28:34 +0059

    cleanup

--- etap_2/code/dox/doc/html/main_8cpp.js
+++
@@ -1,6 +0,0 @@
-var main_8cpp =
-[
-    [ "LINE_SIZE", "main_8cpp.html#aba889888734a8b272a51d444d70ad2fa", null ],
-    [ "ExecPreprocessor", "main_8cpp.html#ac4d3a5e487c32a4061b0ec5059cf8299", null ],
-    [ "main", "main_8cpp.html#a3c04138a5bfe5d72780bb7e82a18e627", null ]
-];
